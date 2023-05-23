9858caafbcf169a3ff6682710c68cfc26ab1ccf9
Akhmadjon <ahmadjonzuhriddinov84@gmail.com>
2023-05-24 02:06:03 +0900

    add login and logout parts

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from  django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -23,3 +24,7 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, "account/dashboard.html", {'section': 'dashboard'})
