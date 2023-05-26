ed29527e511a385d60f685252914620e46160c75
Akhmadjon <ahmadjonzuhriddinov84@gmail.com>
2023-05-26 15:39:11 +0900

    add user registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from  django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -28,3 +28,20 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the User object
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+        
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
