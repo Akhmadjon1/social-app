cd12531672d9c757417041638960d32fd9baff12
Akhmadjon <ahmadjonzuhriddinov84@gmail.com>
2023-07-29 23:44:53 +0900

    end of the chapter

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -152,3 +152,16 @@ SOCIAL_AUTH_TWITTER_SECRET = 'D8pRMmvRKbb7HU85zpWhT1Kxh5wsYzbXY4IeBMz8g2ZQqTcChn
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '780981298992-3auqkdtc4brg98a3n3kg34ohh2psg3i7.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE =[
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
